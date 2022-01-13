a10cd77a6aea212138fd6a23867b6bbaa25c7fac
Tech-Bottle <76836949+Tech-Bottle@users.noreply.github.com>
2022-01-13 15:33:10 +0530

    Revert "This is The configuration for the most exciting tiling WM - Qtile"
    This reverts commit b390086712ff5d26dff7db95c146e27ff9acea7e.

--- qtile/autostart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "Hello this is q-tile tiling window-manager!" | festival --tts &
-picom --experimental-backends --config ~/picom/picom.sample.conf &
-nitrogen --restore &
-conky &
-nm-applet &
